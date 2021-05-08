07c991cd7bb3ba4f2e9d6c7a207b74b8a9a1c19b
tanjed <tanjed.15.6849@gmail.com>
2021-05-08 18:54:58 +0600

    Column added on CLOPLOEngagement

---
+++ database/migrations/2021_05_08_125332_add_column_to_clo_plo_engagement_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnToCloPloEngagementTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('clo_plo_engagements', function (Blueprint $table) {
+            $table->unsignedBigInteger('marking_parameter_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('clo_plo_engagements', function (Blueprint $table) {
+            $table->dropColumn('marking_parameter_id');
+        });
+    }
+}
