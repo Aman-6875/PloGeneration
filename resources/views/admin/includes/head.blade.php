1915faf54c9260685e13074ad15ac0dbc5e4be19
Amanullah Aman <47345468+Aman-6875@users.noreply.github.com>
2021-05-08 22:50:00 +0600

    ...

--- resources/views/admin/includes/head.blade.php
+++ resources/views/admin/includes/head.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
     <meta content="Themesbrand" name="author" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+    <script type="text/javascript" src="jscript/graph.js"></script>
     <!-- App favicon -->
     <link rel="shortcut icon" href="/assets/images/favicon.ico">
 
@@ -26,6 +28,9 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" /> --}}
+    {{-- <script src="https://cdn.anychart.com/releases/8.7.1/js/anychart-core.min.js"></script>
+    <script src="https://cdn.anychart.com/releases/8.7.1/js/anychart-radar.min.js"></script> --}}
+
 
 
 </head>
--- resources/views/plo_generation/plo_table.blade.php
+++ resources/views/plo_generation/plo_table.blade.php
@@ -78,6 +78,8 @@
                                     </div>
 
                                 </div>
+                                <canvas id="marksChart" width="600" height="400"></canvas>
+
                             </div>
                         </div>
                     </div>
@@ -89,5 +91,27 @@
     </div>
     <!-- container-fluid -->
 </div>
+<script>
+
+    var marksCanvas = document.getElementById("marksChart");
+
+    var marksData = {
+    labels: ["English", "Maths", "Physics", "Chemistry", "Biology", "History"],
+    datasets: [{
+        label: "Student A",
+        backgroundColor: "rgba(200,0,0,0.2)",
+        data: [65, 75, 70, 80, 60, 80]
+    }, {
+        label: "Student B",
+        backgroundColor: "rgba(0,0,200,0.2)",
+        data: [54, 65, 60, 70, 70, 75]
+    }]
+    };
+
+var radarChart = new Chart(marksCanvas, {
+  type: 'radar',
+  data: marksData
+});
+</script>
 
 @endsection
