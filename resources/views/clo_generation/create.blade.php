b339cf0e226b7955d792f43c1157f644a0050d52
tanjed <tanjed.15.6849@gmail.com>
2021-05-10 12:00:37 +0600

    Validation Added

--- resources/views/clo_generation/create.blade.php
+++ resources/views/clo_generation/create.blade.php
@@ -77,74 +77,96 @@
                                                     @if ($marking_parameter!= null)
                                                     <td>
 
-                                                        @foreach ($datas as $data)
+                                                        @foreach ($datas as $clo_index => $data)
                                                             <input
                                                                 id="clo-{{$student->id}}-{{$data['clo_id']}}"
                                                                 class="form-control"
                                                                 type="number"
                                                                 name="marks[]"
-                                                            onchange="adjustTotal({{$student->id}},{{$data['clo_id']}})"><br>
-                                                            <input class="form-control" type="hidden"name="clo_id[]" value="{{$data['clo_id']}}">
-                                                            <input class="form-control" type="hidden"name="plo_id[]" value="{{$data['plo_id']}}">
-                                                            <input class="form-control" type="hidden"name="course_id" value="{{ $data['course_id'] }}">
-                                                            <input class="form-control" type="hidden"name="marking_parameter[]" value="{{ $marking_parameter }}">
-                                                            <input class="form-control" type="hidden"name="student_id[]" value="{{ $student->user_id }}">
-                                                            @php
-                                                                $i++;
-                                                            @endphp
-                                                        @endforeach
-                                                    </td>
-                                                    @endif
-                                                    @endforeach
-                                                     <td>
-                                                         @foreach ($datas as $d)
-                                                         <input
-                                                             class="form-control"
-                                                             id="total-{{$student->id}}-{{$d['clo_id']}}"
-                                                             type="number" readonly><br>
-                                                         @endforeach
-                                                     </td>
-                                                </tr>
-                                                 @endforeach
+                                                                placeholder="{{"CLO-".$data['clo_id']}}"
+                                                            onchange="adjustTotal({{$student->id}},{{$data['clo_id']}},{{$marking_parameter}},{{json_encode($data)}})"
+                                                            <?php
+                                                                $marking_parameter_index = array_search($marking_parameter,$data['marking_parameter']);
+                                                                $given_mark = $data['input_number'][$marking_parameter_index];
+                                                                if (!$given_mark) echo 'readonly';
+                                                            ?>
+                                                            ><br>
+<input class="form-control" type="hidden"name="clo_id[]" value="{{$data['clo_id']}}">
+<input class="form-control" type="hidden"name="plo_id[]" value="{{$data['plo_id']}}">
+<input class="form-control" type="hidden"name="course_id" value="{{ $data['course_id'] }}">
+<input class="form-control" type="hidden"name="marking_parameter[]" value="{{ $marking_parameter }}">
+<input class="form-control" type="hidden"name="student_id[]" value="{{ $student->user_id }}">
+@php
+    $i++;
+@endphp
+@endforeach
+</td>
+@endif
+@endforeach
+<td>
+@foreach ($datas as $d)
+<input
+ class="form-control"
+ id="total-{{$student->id}}-{{$d['clo_id']}}"
+ type="number" readonly><br>
+@endforeach
+</td>
+</tr>
+@endforeach
 
 
 
 
 
-                                                </tbody>
-                                            </table>
-                                        </div>
+</tbody>
+</table>
+</div>
 
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+</div>
+</div>
+</div>
+</div>
 
-                        <div class="row justify-content-center">
-                            <div class="col-sm-9">
-                                <center>
-                                    <button type="submit" class="btn btn-primary w-50">Save</button>
-                                </center>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <!-- end card body -->
-            </div>
-        </div>
-        <!-- end row -->
-    </div>
-    <!-- container-fluid -->
+<div class="row justify-content-center">
+<div class="col-sm-9">
+<center>
+<button type="submit" class="btn btn-primary w-50">Save</button>
+</center>
+</div>
+</div>
+</form>
+</div>
+<!-- end card body -->
+</div>
+</div>
+<!-- end row -->
+</div>
+<!-- container-fluid -->
 </div>
 
 <script>
-    function adjustTotal(student_id,id){
-        let element_value = document.getElementById('clo-'+student_id+'-'+id)
-        let element_total = document.getElementById('total-'+student_id+'-'+id)
-        let total_value = parseFloat(element_total.value)
-        if (isNaN(total_value)) total_value = 0
-        total_value+= parseFloat(element_value.value)
-        element_total.value = total_value
+function adjustTotal(student_id,id,marking_parameter,data){
+let element_value = document.getElementById('clo-'+student_id+'-'+id)
+let element_total = document.getElementById('total-'+student_id+'-'+id)
+    let is_okay = cloValidation(marking_parameter,data,parseFloat(element_value.value))
+    if (!is_okay) element_value.value = ''
+let total_value = parseFloat(element_total.value)
+if (isNaN(total_value)) total_value = 0
+total_value+= parseFloat(element_value.value)
+element_total.value = total_value
+}
+
+function cloValidation(marking_parameter,data,value)
+{
+    let marking_parameters = data.marking_parameter
+    let input_numbers = data.input_number
+    let marking_param_index = marking_parameters.indexOf(String(marking_parameter))
+    if (value > input_numbers[marking_param_index]){
+        alert('Value can not be greater than '+ input_numbers[marking_param_index]);
+        return false
     }
+    return true
+
+}
 </script>
 @endsection
